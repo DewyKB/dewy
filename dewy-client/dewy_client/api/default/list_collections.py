d518cdc8ce30faea744fe04d03b2065366a7abf5
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2024-02-01 11:56:13 -0800

    feat: Allow listing collection by name (#62)
    * feat: Allow listing collection by name
    
    This allows clients to easily locate a collection ID given a name.
    
    * lint / format
    
    * update openapi spec

--- dewy-client/dewy_client/api/default/list_collections.py
+++ dewy-client/dewy_client/api/default/list_collections.py
@@ -6,13 +6,29 @@ import httpx
 from ... import errors
 from ...client import AuthenticatedClient, Client
 from ...models.collection import Collection
-from ...types import Response
+from ...models.http_validation_error import HTTPValidationError
+from ...types import UNSET, Response, Unset
 
 
-def _get_kwargs() -> Dict[str, Any]:
+def _get_kwargs(
+    *,
+    name: Union[None, Unset, str] = UNSET,
+) -> Dict[str, Any]:
+    params: Dict[str, Any] = {}
+
+    json_name: Union[None, Unset, str]
+    if isinstance(name, Unset):
+        json_name = UNSET
+    else:
+        json_name = name
+    params["name"] = json_name
+
+    params = {k: v for k, v in params.items() if v is not UNSET and v is not None}
+
     _kwargs: Dict[str, Any] = {
         "method": "get",
         "url": "/api/collections/",
+        "params": params,
     }
 
     return _kwargs
@@ -20,7 +36,7 @@ def _get_kwargs() -> Dict[str, Any]:
 
 def _parse_response(
     *, client: Union[AuthenticatedClient, Client], response: httpx.Response
-) -> Optional[List["Collection"]]:
+) -> Optional[Union[HTTPValidationError, List["Collection"]]]:
     if response.status_code == HTTPStatus.OK:
         response_200 = []
         _response_200 = response.json()
@@ -30,6 +46,10 @@ def _parse_response(
             response_200.append(response_200_item)
 
         return response_200
+    if response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY:
+        response_422 = HTTPValidationError.from_dict(response.json())
+
+        return response_422
     if client.raise_on_unexpected_status:
         raise errors.UnexpectedStatus(response.status_code, response.content)
     else:
@@ -38,7 +58,7 @@ def _parse_response(
 
 def _build_response(
     *, client: Union[AuthenticatedClient, Client], response: httpx.Response
-) -> Response[List["Collection"]]:
+) -> Response[Union[HTTPValidationError, List["Collection"]]]:
     return Response(
         status_code=HTTPStatus(response.status_code),
         content=response.content,
@@ -50,20 +70,26 @@ def _build_response(
 def sync_detailed(
     *,
     client: Union[AuthenticatedClient, Client],
-) -> Response[List["Collection"]]:
+    name: Union[None, Unset, str] = UNSET,
+) -> Response[Union[HTTPValidationError, List["Collection"]]]:
     """List Collections
 
      List collections.
+
+    Args:
+        name (Union[None, Unset, str]): Find collections by name.
 
     Raises:
         errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
         httpx.TimeoutException: If the request takes longer than Client.timeout.
 
     Returns:
-        Response[List['Collection']]
+        Response[Union[HTTPValidationError, List['Collection']]]
     """
 
-    kwargs = _get_kwargs()
+    kwargs = _get_kwargs(
+        name=name,
+    )
 
     response = client.get_httpx_client().request(
         **kwargs,
@@ -75,41 +101,52 @@ def sync_detailed(
 def sync(
     *,
     client: Union[AuthenticatedClient, Client],
-) -> Optional[List["Collection"]]:
+    name: Union[None, Unset, str] = UNSET,
+) -> Optional[Union[HTTPValidationError, List["Collection"]]]:
     """List Collections
 
      List collections.
+
+    Args:
+        name (Union[None, Unset, str]): Find collections by name.
 
     Raises:
         errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
         httpx.TimeoutException: If the request takes longer than Client.timeout.
 
     Returns:
-        List['Collection']
+        Union[HTTPValidationError, List['Collection']]
     """
 
     return sync_detailed(
         client=client,
+        name=name,
     ).parsed
 
 
 async def asyncio_detailed(
     *,
     client: Union[AuthenticatedClient, Client],
-) -> Response[List["Collection"]]:
+    name: Union[None, Unset, str] = UNSET,
+) -> Response[Union[HTTPValidationError, List["Collection"]]]:
     """List Collections
 
      List collections.
+
+    Args:
+        name (Union[None, Unset, str]): Find collections by name.
 
     Raises:
         errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
         httpx.TimeoutException: If the request takes longer than Client.timeout.
 
     Returns:
-        Response[List['Collection']]
+        Response[Union[HTTPValidationError, List['Collection']]]
     """
 
-    kwargs = _get_kwargs()
+    kwargs = _get_kwargs(
+        name=name,
+    )
 
     response = await client.get_async_httpx_client().request(**kwargs)
 
@@ -119,21 +156,26 @@ async def asyncio_detailed(
 async def asyncio(
     *,
     client: Union[AuthenticatedClient, Client],
-) -> Optional[List["Collection"]]:
+    name: Union[None, Unset, str] = UNSET,
+) -> Optional[Union[HTTPValidationError, List["Collection"]]]:
     """List Collections
 
      List collections.
+
+    Args:
+        name (Union[None, Unset, str]): Find collections by name.
 
     Raises:
         errors.UnexpectedStatus: If the server returns an undocumented status code and Client.raise_on_unexpected_status is True.
         httpx.TimeoutException: If the request takes longer than Client.timeout.
 
     Returns:
-        List['Collection']
+        Union[HTTPValidationError, List['Collection']]
     """
 
     return (
         await asyncio_detailed(
             client=client,
+            name=name,
         )
     ).parsed
