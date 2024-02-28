d58f9ee4d748da6011e8918b5be70edef054795e
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2024-02-28 08:08:25 -0800

    formatting

--- demos/python-langchain-cli/python_langchain_cli/__main__.py
+++ demos/python-langchain-cli/python_langchain_cli/__main__.py
@@ -2,8 +2,8 @@ import os
 import sys
 
 import click
-
 from dewy_client import Client
+
 
 @click.group()
 @click.option("--collection", default="main")
@@ -14,8 +14,9 @@ def cli(ctx, collection, base_url):
     # by means other than the `if` block below)
     ctx.ensure_object(dict)
 
-    ctx.obj['base_url'] = base_url
-    ctx.obj['collection'] = collection
+    ctx.obj["base_url"] = base_url
+    ctx.obj["collection"] = collection
+
 
 @cli.command()
 @click.pass_context
@@ -25,13 +26,13 @@ def add_file(ctx, url_or_file):
     from dewy_client.models import AddDocumentRequest, BodyUploadDocumentContent
     from dewy_client.types import File
 
-    client = Client(ctx.obj['base_url'])
+    client = Client(ctx.obj["base_url"])
     if os.path.isfile(url_or_file):
         document = add_document.sync(
             client=client,
             body=AddDocumentRequest(
-                collection = ctx.obj['collection'],
-            )
+                collection=ctx.obj["collection"],
+            ),
         )
         print(f"Added document {document.id}. Uploading content.")
 
@@ -52,35 +53,37 @@ def add_file(ctx, url_or_file):
     else:
         document = add_document.sync(
             client=client,
-            body=AddDocumentRequest(
-                collection = ctx.obj['collection'],
-                url=url_or_file
-            )
+            body=AddDocumentRequest(collection=ctx.obj["collection"], url=url_or_file),
         )
         print(f"Added document {document.id} from URL '{url_or_file}'")
 
+
 @cli.command()
 @click.pass_context
-@click.argument("query", type=click.File('r'), default=sys.stdin)
+@click.argument("query", type=click.File("r"), default=sys.stdin)
 def query(ctx, query):
     from dewy_langchain import DewyRetriever
+    from langchain_core.output_parsers import StrOutputParser
     from langchain_core.prompts import ChatPromptTemplate
     from langchain_core.runnables import RunnablePassthrough
-    from langchain_core.output_parsers import StrOutputParser
     from langchain_openai import ChatOpenAI
 
-    retriever = DewyRetriever.for_collection(collection=ctx.obj['collection'], base_url=ctx.obj['base_url'])
+    retriever = DewyRetriever.for_collection(
+        collection=ctx.obj["collection"], base_url=ctx.obj["base_url"]
+    )
 
-    prompt = ChatPromptTemplate.from_template("""Answer the question based only on the following context:
+    prompt = ChatPromptTemplate.from_template(
+        """Answer the question based only on the following context:
     {context}
 
     Question: {question}
-    """)
+    """
+    )
 
     model = ChatOpenAI()
 
     chain = (
-        { "context": retriever, "question": RunnablePassthrough() }
+        {"context": retriever, "question": RunnablePassthrough()}
         | prompt
         | model
         | StrOutputParser()
@@ -90,6 +93,7 @@ def query(ctx, query):
     print(f"Invoking chain for:\n{query_str}")
     result = chain.invoke(query_str)
     print(f"\n\nAnswer:\n{result}")
+
 
 if __name__ == "__main__":
     cli()
